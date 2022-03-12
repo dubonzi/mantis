4a6e52cc20feb9e4104754d0dcb7a90117d39581
Eduardo Bonzi <eduardobonzi.dev@gmail.com>
2022-03-12 16:51:54 -0300

    feat: mapping request from fiber

--- pkg/app/handler.go
+++ pkg/app/handler.go
@@ -1,6 +1,30 @@
 package app
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+type Request struct {
+	Path    string
+	Method  string
+	Headers map[string]string
+	Body    string
+}
+
+func RequestFromFiber(r *fiber.Request) Request {
+	req := Request{
+		Path:    string(r.URI().RequestURI()),
+		Body:    string(r.Body()),
+		Method:  string(r.Header.Method()),
+		Headers: make(map[string]string),
+	}
+	r.Header.VisitAll(
+		func(key, value []byte) {
+			req.Headers[string(key)] = string(value)
+		},
+	)
+	return req
+}
 
 type Handler struct {
 	matcher *Matcher
@@ -11,5 +35,5 @@ func NewHandler(matcher *Matcher) *Handler {
 }
 
 func (h Handler) All(c *fiber.Ctx) error {
-	return c.SendString("Hello")
+	return c.JSON(RequestFromFiber(c.Request()))
 }
---
+++ pkg/app/handler_test.go
@@ -0,0 +1,73 @@
+package app
+
+import (
+	"testing"
+
+	"github.com/go-playground/assert/v2"
+	"github.com/gofiber/fiber/v2"
+)
+
+func TestRequest(t *testing.T) {
+
+	tests := []struct {
+		name  string
+		input *fiber.Request
+		want  Request
+	}{
+		{
+			name: "Should build POST request",
+			input: func() *fiber.Request {
+				r := &fiber.Request{}
+				r.SetBodyString("{\"name\": \"gopher 1\"}")
+				r.Header.Add("Content-Type", "application/json")
+				r.Header.SetMethod("POST")
+				r.SetRequestURI("/gopher")
+				return r
+			}(),
+			want: Request{
+				Method:  "POST",
+				Path:    "/gopher",
+				Headers: map[string]string{"Content-Type": "application/json"},
+				Body:    "{\"name\": \"gopher 1\"}",
+			},
+		},
+		{
+			name: "Should build GET request",
+			input: func() *fiber.Request {
+				r := &fiber.Request{}
+				r.Header.Add("Accept", "application/json")
+				r.Header.SetMethod("GET")
+				r.SetRequestURI("/gopher/2")
+				return r
+			}(),
+			want: Request{
+				Method:  "GET",
+				Path:    "/gopher/2",
+				Headers: map[string]string{"Accept": "application/json"},
+			},
+		},
+		{
+			name: "Should build request with no headers",
+			input: func() *fiber.Request {
+				r := &fiber.Request{}
+				r.Header.SetMethod("GET")
+				r.SetRequestURI("/gopher/2")
+				return r
+			}(),
+			want: Request{
+				Method:  "GET",
+				Path:    "/gopher/2",
+				Headers: map[string]string{},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+
+		t.Run(tt.name, func(t *testing.T) {
+			got := RequestFromFiber(tt.input)
+			assert.Equal(t, got, tt.want)
+		})
+	}
+
+}

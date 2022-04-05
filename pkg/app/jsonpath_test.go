5fe9addc4278eb601dfd06814d81adc272cb021d
Eduardo Bonzi <eduardobonzi.dev@gmail.com>
2022-04-04 21:47:16 -0300

    tests: improve coverage of the jsonpath cache

--- pkg/app/jsonpath_test.go
+++ pkg/app/jsonpath_test.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/go-playground/assert/v2"
@@ -8,38 +9,51 @@ import (
 
 func TestJSONPathCache(t *testing.T) {
 	tests := []struct {
-		expression string
-		wantErr    bool
+		expressions []string
+		wantErr     bool
+		wantLen     int
 	}{
 		{
-			expression: `$[?(@.product.id == '12345')]`,
-			wantErr:    false,
+			expressions: []string{`$[?(@.product.id == '12345')]`},
+			wantErr:     false,
+			wantLen:     1,
 		},
 		{
-			expression: `$.person[?(@.age > 21 || @.name == 'John')]`,
-			wantErr:    false,
+			expressions: []string{`$.person[?(@.age > 21 || @.name == 'John')]`, `$.name`},
+			wantErr:     false,
+			wantLen:     2,
 		},
 		{
-			expression: `$.person.[a]`,
-			wantErr:    true,
+			expressions: []string{`$[?(@.product.id == '12345')]`, `$[?(@.product.id == '12345')]`},
+			wantErr:     false,
+			wantLen:     1,
 		},
 		{
-			expression: `$.person[?(@.age > 21 && name == 'John')]`,
-			wantErr:    true,
+			expressions: []string{`$.person.[a]`},
+			wantErr:     true,
+			wantLen:     1,
+		},
+		{
+			expressions: []string{`$.person[?(@.age > 21 && name == 'John')]`},
+			wantErr:     true,
+			wantLen:     1,
 		},
 	}
 
-	for _, tt := range tests {
-		jc := NewJSONPathCache()
-		err := jc.AddExpressions([]string{tt.expression})
-		if !assert.IsEqual(err != nil, tt.wantErr) {
-			t.Logf("error parsing jsonpath expression: %s , %s", tt.expression, err)
-			t.Fail()
-		}
+	for index, tt := range tests {
+		t.Run(fmt.Sprint(index), func(t *testing.T) {
 
-		if !tt.wantErr {
-			assert.Equal(t, len(jc.cache), 1)
-		}
+			jc := NewJSONPathCache()
+			err := jc.AddExpressions(tt.expressions)
+			if !assert.IsEqual(err != nil, tt.wantErr) {
+				t.Logf("error parsing jsonpath expression: %s , %s", tt.expressions, err)
+				t.FailNow()
+			}
+
+			if !tt.wantErr {
+				assert.Equal(t, len(jc.cache), tt.wantLen)
+			}
+		})
 	}
 
 }
